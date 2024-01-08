d48d61615345a54ca98af5149695e4c2f6977912
Amr <frank1276smith@gmail.com>
2024-01-08 10:56:13 +0200

    adding another alternative approach to use react routing but we will stick to another approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+import Home from "./pages/Home.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
+
+/* const routerDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+); */
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
+// const router = createBrowserRouter(routerDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
